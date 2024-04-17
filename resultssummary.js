8592d843b5be1e86a216aca0b74db0364511196a
Melissa Martins <melmartins.pro@gmail.com>
2024-04-17 11:32:52 -0300

    Changes and estilization

--- resultssummary.js
+++ resultssummary.js
@@ -4,7 +4,7 @@ function carregar() {
     .then((data) => {
       const resultado = document.querySelector("#results");
       
-      data.map((d) => {
+      data.map((d,key) => {
         const cardsummary = document.createElement("div");
         cardsummary.classList.add("card-summary");
 
@@ -17,12 +17,18 @@ function carregar() {
         const score = document.createElement('p')
         score.textContent = d.score
 
+        //i am adding the weight of the font and the color using a id on the const score
+        score.setAttribute('id', 'score')
+        //here i am using a setAttribute to change dinamycally the styles of each results
+         cardsummary.setAttribute("id",'resultscss'+key)
         cardsummary.appendChild(icon)
         cardsummary.appendChild(categoria)
         cardsummary.appendChild(score)
         resultado.appendChild(cardsummary)
+
         cardsummary.innerHTML += '<p>/100</p>'
       });
+      
     
     });
     
